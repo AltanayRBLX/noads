a899b51b17bd12621b16be0bfc55ed122763d4c6
AltanayRBLX <35113904+AltanayRBLX@users.noreply.github.com>
2018-01-06 20:14:56 +0300

    Update noads.js

--- noads.js
+++ noads.js
@@ -1,10 +1,10 @@
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
+var prefix = 'na!'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame("No Ads | 1/1 | " + " Made by Cube Development.", 'https://www.twitch.tv/turkishtr2', 1);
+  client.user.setGame("No Ads | 1/1 | " + " na!yardım.", 'https://www.twitch.tv/turkishtr2', 1);
 });
 
 client.on('message', msg => {
@@ -14,6 +14,9 @@ client.on('message', msg => {
             msg.delete()
             msg.channel.send("<@" + msg.author.id + ">, **Bence reklam yapmayı bırak.Çünkü ben bu iş için görevlendirildim.**");
         }
+    }
+    if (msg.content.toLowerCase() === prefix + 'yardım') {
+     msg.channel.send('Bu bot bir Cube projesidir tüm hakları saklıdır.Yapabildiklerim ise:Discord Sunucu Reklamlarından Sunucunuzu koruma altına alırım.')
     }
 });
 
