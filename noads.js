7ecfc4956f658b186d13b9d5a6611d8d9f4b857b
AltanayRBLX <35113904+AltanayRBLX@users.noreply.github.com>
2018-01-07 13:14:19 +0300

    Update noads.js

--- noads.js
+++ noads.js
@@ -19,14 +19,7 @@ client.on('message', msg => {
 
     if (msg.content.toLowerCase() === prefix + 'yardım') {
      msg.channel.send('Bu bot bir Cube projesidir tüm hakları saklıdır.Yapabildiklerim ise:Discord Sunucu Reklamlarından Sunucunuzu koruma altına alırım.')
-    }
-    if (msg.content.toLowerCase().indexOf("https:/") > -1) {
-       if (!msg.author.bot) false
-           msg.react("😡")
-           msg.delete()
-           msg.channel.send("<@" + msg.author.id + ">, **Bence reklam yapmayı bırak.Çünkü ben bu iş için görevlendirildim.**");
-        }
-    }
+}
 
     //Ver
 });
